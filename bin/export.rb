b1f115061cf6d61dadc51918f53eb829c8b913c4
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-10-02 14:17:47 -0400

    First pass at iterating over stages that we want to export

--- bin/export.rb
+++ bin/export.rb
@@ -1,10 +1,15 @@
 #!/usr/local/bin/ruby -w
 require 'nypl_log_formatter'
+require 'yaml'
 
+# Get useful paths
 WORKSPACE_DIR = '/opt/git_working_directory'.freeze
 repository_dir_name = File.basename(ENV['EXPORT_GIT_URL'], '.*')
 FULL_PATH_TO_REPO_DIR = File.join(WORKSPACE_DIR, repository_dir_name)
 
+# Load stages.yml
+stages = YAML.load_file(File.join(__dir__, '..', 'stages.yml'))
+require 'pry'; binding.pry;
 logger = NyplLogFormatter.new(STDOUT)
 
 # Clone the git repo where the backups are stored
@@ -12,20 +17,20 @@ logger.info("Cloning #{ENV['EXPORT_GIT_URL']} to #{WORKSPACE_DIR}")
 `cd #{WORKSPACE_DIR} && git clone #{ENV['EXPORT_GIT_URL']}`
 
 # Thar be psudocode below
-# stages.each do |stage|
-#   filename = File.join(repository_dir_name, stage[:name])
-#   if !file_exist?
-#     logger.info("#{filename}.json doesn't exist, saving first copy")
-#     write file
-#     add/commit/push
-#   else
-#     export latest API.
-#     if different?
-#       logger.info("#{filename} has changed, comitting changes")
-#       write file
-#       add/commit/push
-#     else
-#       logger.info("#{filename} has not changed since last run")
-#     end
-#   end
-# end
+stages.each do |stage|
+  path_to_stage_export = File.join(WORKSPACE_DIR, "#{stage['name']}.json")
+  if !File.exist?(path_to_stage_export)
+    logger.info("#{path_to_stage_export}.json doesn't exist, saving first copy")
+    # write file
+    # add/commit/push
+  else
+    # export latest API.
+    # if different?
+    #   logger.info("#{filename} has changed, comitting changes")
+    #   write file
+    #   add/commit/push
+    # else
+    #   logger.info("#{filename} has not changed since last run")
+    # end
+  end
+end
