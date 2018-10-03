51d92b0da8b328765e769129df9ab4cf80b5f635
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-10-02 21:43:08 -0400

    Actually push changes

--- bin/export.rb
+++ bin/export.rb
@@ -1,4 +1,4 @@
-#!/usr/local/bin/ruby -w
+#!/usr/local/bin/ruby
 require 'nypl_log_formatter'
 require 'aws-sdk-apigateway'
 require 'json'
@@ -22,7 +22,6 @@ def get_agent(stage)
   Aws::APIGateway::Client.new(options)
 end
 
-#
 def get_export_for(stage, api_gateway_client)
   resp = api_gateway_client.get_export(
     rest_api_id: stage['rest_api_id'],
@@ -37,15 +36,14 @@ def get_export_for(stage, api_gateway_client)
 end
 
 def write_and_commit(options = {})
-  # logger.info("writing export of stage #{options['stage']} to #{options['path_to_file']}")
   File.open(options[:path_to_file], 'w') {|f| f.write(options[:export_string]) }
-  `cd #{FULL_PATH_TO_REPO_DIR} && git commit -am 'Updating contents of #{File.basename(options[:path_to_file], '.*')}'`
-  # TODO: Git push
+  `cd #{FULL_PATH_TO_REPO_DIR} && git add #{File.basename(options[:path_to_file])} && git commit -am 'Updating contents of #{File.basename(options[:path_to_file], '.*')}.json'`
+  # `cd #{FULL_PATH_TO_REPO_DIR} && git push origin master`
 end
 
 # Clone the git repo where the backups are stored
 logger.info("Cloning #{ENV['EXPORT_GIT_URL']} to #{WORKSPACE_DIR}")
-`cd #{WORKSPACE_DIR} && git clone #{ENV['EXPORT_GIT_URL']}`
+`cd #{WORKSPACE_DIR} && rm -rfd #{repository_dir_name} && git clone #{ENV['EXPORT_GIT_URL']}`
 
 # Thar be psudocode below
 stages.each do |stage|
@@ -55,10 +53,8 @@ stages.each do |stage|
     logger.info("#{path_to_stage_export}.json doesn't exist, saving first copy")
     export = get_export_for(stage, api_gateway_client)
     write_and_commit(stage: stage, export_string: export, path_to_file: path_to_stage_export)
-    # write file
-    # add/commit/push
   else
-    # export latest API.
+    logger.info("#{path_to_stage_export}.json exists. Checking to see if it's been updated since last run")
     # if different?
     #   logger.info("#{filename} has changed, comitting changes")
     #   write file
